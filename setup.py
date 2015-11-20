6fd9a5ee545720637eef56fbae47864be75ab5e6
Bruno Dupuis <bruno.dupuis@novapost.fr>
2015-11-20 11:35:28 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+VERSION = '0.2'
+
 setup(
   name = 'django_macfly',
-  packages = ['django_macfly'],
-  version = '0.1',
+  packages = ['django_macfly',
+              "django_macfly.management",
+              "django_macfly.management.commands"],
+  version = VERSION,
   description = 'Django helpers to run SQL schema migrations ahead of time.',
   author = 'Bruno Dupuis',
   author_email = 'bruno.dupuis@people-doc.com',
   url = 'https://github.com/novafloss/django-MacFly',
-  download_url = 'https://github.com/novafloss/django-MacFly/tarball/0.1',
+  download_url = 'https://github.com/novafloss/django-MacFly/tarball/%s' % VERSION,
   keywords = ['django', 'migrations', 'DBA'],
   classifiers = [],
 )
